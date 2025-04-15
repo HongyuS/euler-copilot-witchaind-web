e5b40f644d0b89027df7d912b607932d7453e91d
李娜 <lnlnln629@163.com>
2025-04-15 20:06:00 +0800

    手动更改路由时不被缓存覆盖

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,36 @@ import '@/styles/app.scss';
 import router from './router';
 
 const locale = computed(() => appStore.locale);
-
+// 新增路由状态标记
+const isManualNavigation = ref(false);
 onMounted(() => {
+  // 监听手动导航
+  router.beforeEach((to, from, next) => {
+    if (from.name === undefined && to.name !== 'root') {
+      isManualNavigation.value = true;
+    }
+    next();
+  });
   window.addEventListener('beforeunload', () => {
-    sessionStorage.setItem('beforeunload', '1');
+    if (!isManualNavigation.value) {
+      sessionStorage.setItem('routerName', router.currentRoute.value.name?.toString() || '');
+    }
+    sessionStorage.setItem('beforeunload', 'true');
   });
   // 监听页面刷新
   const beforeUnload = sessionStorage.getItem('beforeunload');
-  if (beforeUnload === '1') {
+  if (beforeUnload === 'true') {
+    if (!isManualNavigation.value) {
     const name = sessionStorage.getItem('routerName');
     if (name) {
       router.push(name); // 如果sessionStorage存在路由，去缓存的路由
     } else {
       router.push('/'); // 不存在存储，去主页
     }
+  }
     sessionStorage.removeItem('beforeunload'); // 清除标记
+    sessionStorage.removeItem('routerName');
+    isManualNavigation.value = false;
   }
 });
 </script>
