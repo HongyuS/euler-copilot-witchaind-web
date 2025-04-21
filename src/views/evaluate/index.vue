8b00660ed86310d6a6bb03ac25a51c50351364f6
李娜 <lnlnln629@163.com>
2025-04-21 10:31:38 +0800

    资产库内标签页初始化

---
+++ src/views/evaluate/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="evaluate-empty-content">
+        <EmptyStatus description="暂无评测信息，去数据集生成一个吧！" buttonText="去生成评测" buttonClass="group-btn" @click="handleCreate" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import "@/styles/evaluate.scss"
+import EmptyStatus from '@/components/EmptyStatus/index.vue'
+
+const handleCreate = () => {
+}
+
+</script>
